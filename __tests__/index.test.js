eae1f0c883eb910defb12ab9974164a47cae38bc
RomanTsvetik <zvetik85@gmail.com>
2022-01-13 11:30:20 +0200

    hw 12 - update of hw 11 files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,11 @@
-const { sum, reversedString } = require('../src/index')
+const {
+    sum,
+    reversedString,
+    getIndexOf,
+    getLastIndexOf,
+    getIncludes,
+    getSomeEl
+} = require("../src/index");
 
 test('Sum should return sum of two values', () => {
     expect(sum(1, 3)).toBe(4)
@@ -15,4 +22,33 @@ test("Sum should return value correctly comparing to other", () => {
 test("reversedString", () => {
     expect(reversedString("hello")).toEqual("olleh");
     expect(reversedString("")).toEqual("");
+});
+
+
+test('getIndexOf should return index of elem in array or -1 if it is not found', () => {
+    expect(getIndexOf([3, 5, 10, 2], 5)).toBe(1)
+    expect(getIndexOf([3, 5, 10, 2], 4)).toEqual(-1);
+    expect(getIndexOf([3, 5, 10, 2], 10)).not.toEqual(-1);
+});
+
+test('getLastIndexOf should return last index of elem in array or -1 if no one is found', () => {
+    expect(getLastIndexOf([3, 4, 6, 5, 4], 4)).toBe(4)
+    expect(getLastIndexOf(['Dodo', 'Tiger', 'Penguin', 'Dodo'], 'Jerry')).toEqual(-1)
+    expect(getLastIndexOf([3, 4, 6, 5, 4], 4)).not.toBe(-1);
+})
+
+test('getIncludes should return true or false depending on array contains the elem', () => {
+    expect(getIncludes(["cat", "dog", "bat"], "cat")).toBe(true);
+    expect(getIncludes([2, 5, 6, 9], 3)).toBeFalsy();
+    expect(getIncludes([2, 5, 6, 9], 9)).toBeTruthy();
+})
+
+
+const even = (element) => element % 2 === 0;
+const isBiggerThanFive = (el) => el > 5;
+
+test("getSomeEl should return true if any elem of arr passes the test implemented by callback func", () => {
+    expect(getSomeEl(even)).toBeFalsy();
+    expect(getSomeEl(isBiggerThanFive)).toBeTruthy()
+    expect(getSomeEl(isBiggerThanFive)).toEqual(true)
 });
--- src/index.js
+++ src/index.js
@@ -24,4 +24,77 @@ const reversedString = str => str.split('').reverse().join(''); // без оши
 const nativeNull = () => null;
 
 // expect(sum(41, 1)).toBe(44)
-module.exports = { sum, nativeNull, reversedString }
+
+const getIndexOf = (arr, el) => {
+    let fromIndex = 0;
+    let index = -1;
+    let i = 0;
+
+    if (fromIndex >= arr.length) return index;
+
+    if (fromIndex < 0) i = arr.length + fromIndex;
+
+    for (i; i < arr.length; i++){
+        if (el === arr[i]) {
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+}
+
+const getLastIndexOf = (arr, el) => {
+    
+    let fromIndex = arr.length;
+    let index = -1;
+
+    if (fromIndex < 0) fromIndex = 0;
+    if (fromIndex > arr.length) fromIndex = arr.length;
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (el === arr[i]) {
+            index = i;
+            break;
+        }
+    } 
+    return index;
+
+}
+
+
+const getIncludes = (arr, el) => {
+    let bool = false;
+    let fromIndex = 0;
+
+    if (fromIndex >= arr.length) return bool;
+    if (fromIndex < 0) i = arr.length + fromIndex;
+    
+    arr.forEach(i => {
+        if (el === i) bool = true;
+    });
+    return bool;
+}
+
+
+const arr = [1, 7, 3, 9, 5];
+const getSomeEl = (callback) => {
+    if (arr.length === 0) return false;
+
+    for (let i = 0; i < arr.length; i++){
+        if (callback(arr[i])) return true;
+    }
+    return false;
+}
+
+
+module.exports = {
+    sum,
+    nativeNull,
+    reversedString,
+    getIndexOf,
+    getLastIndexOf,
+    getIncludes,
+    getSomeEl,
+};
+
